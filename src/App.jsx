8ce49b8ad2e3bf54c3bb200808d57562ec06d6c8
K-thun <kauakalel25@gmail.com>
2026-02-01 21:38:06 -0300

    Commit final

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,31 @@ import SearchBar from "./components/SearchBar";
 
 const listCars = [
   {
-	id: 1,
-	brand: "Hyundai",
-	model: "HB20",
-	km: "65.000",
-	category: "Hatch",
-	year: "2018",
-	price: "55.000,00",
+    id: 1,
+    brand: "Hyundai",
+    model: "HB20",
+    km: "65.000",
+    category: "Hatch",
+    year: "2018",
+    price: "55.000,00",
   },
   {
-	id: 2,
-	brand: "Toyota",
-	model: "Hilux",
-	km: "100.000",
-	category: "Picape",
-	year: "2019",
-	price: "155.000,00",
+    id: 2,
+    brand: "Toyota",
+    model: "Hilux",
+    km: "100.000",
+    category: "Picape",
+    year: "2019",
+    price: "155.000,00",
   },
   {
-	id: 3,
-	brand: "Volkswagen",
-	model: "T-Cross",
-	km: "20.000",
-	category: "SUV",
-	year: "2021",
-	price: "135.000,00",
+    id: 3,
+    brand: "Volkswagen",
+    model: "T-Cross",
+    km: "20.000",
+    category: "SUV",
+    year: "2021",
+    price: "135.000,00",
   },
 ];
 
@@ -45,7 +45,7 @@ const App = () => {
        return;
      }
 
-	    setCars(listCars.filter(car => car.model.includes(query)));
+        setCars(listCars.filter(car => car.model.includes(query)));
    }
 
    return (
